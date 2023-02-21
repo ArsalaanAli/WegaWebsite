96fe9a87ec78ba2cafc7cb102397723a89072ee3
elyknehc <kchen727@uwo.ca>
2023-02-21 00:32:15 -0500

    added navbir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
 	return (
@@ -9,8 +10,10 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
 			<main>
-				<h1 className="text-3xl font-bold underline">Hello World!</h1>{" "}
+				<Navbar />
+				<h1 className="text-3xl font-bold underline">Hello World!</h1>
 			</main>
 		</>
 	);

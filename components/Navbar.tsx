2395731ce3833a257bc50af68b4887308fc10773
elyknehc <kchen727@uwo.ca>
2023-02-21 21:23:22 -0500

    materials UI added for footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Logo from "../public/favicon.ico";
 
 const navbar = () => {
-  return <div>Hello</div>;
+	return <div>Hello</div>;
 };
 
 export default navbar;

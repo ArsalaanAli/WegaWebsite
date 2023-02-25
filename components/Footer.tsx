b3f6cde4b015a1ba896f63544732d9ff08f90277
Arsalaan Ali <arsalaanali2@gmail.com>
2023-02-24 23:34:28 -0500

    basic navbar layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { Twitter } from "@mui/icons-material";
-import { YouTube } from "@mui/icons-material";
-import { Instagram } from "@mui/icons-material";
-import { Facebook } from "@mui/icons-material";
 
 function Footer() {
 	return (
 		<div className="">
-			<Twitter />
-			<YouTube />
-			<Instagram />
-			<Facebook />
-			{/*Add Discord, TikTok*/}
 		</div>
 	);
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,28 @@ import Image from "next/image";
 import Logo from "../public/favicon.ico";
 
 const navbar = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <nav className="flex justify-between px-20 py-10 items-center bg-white">
+        <div className="flex items-center">
+          <Image
+            src="/favicon.ico"
+            alt="wega logo"
+            width={60}
+            height={60}
+            className="pr-4"
+          />
+          <h1 className="text-xl text-gray-800 font-bold">WEGA</h1>
+        </div>
+        <div className="flex items-center">
+          <ul className="flex items-center space-x-6">
+            <li className="font-semibold text-gray-700">Home</li>
+            <li className="font-semibold text-gray-700">Articles</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default navbar;

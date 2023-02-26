4e81e1c14b325666c1255fb2b728a77765798a29
elyknehc <kchen727@uwo.ca>
2023-02-25 21:55:24 -0500

    update home page and navbar colours

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -4,8 +4,11 @@ import banner from "../public/wegabanner.png";
 
 function HomePage() {
 	return (
-		<div className="w-full h-screen">
+		<div>
 			<Image src={banner} alt="WEGA banner" />
+			<h1 className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 truncate text-4xl text-black ">
+				Westerns Leading Gaming Community
+			</h1>
 		</div>
 	);
 }

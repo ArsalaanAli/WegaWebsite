7fb57d07e2b808bd48ad0a403f7e829cf5b88b61
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2023-02-28 22:44:14 -0500

    nav bar colour scheme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 function Footer() {
-	return (
-		<footer className="bg-gray-900 text-white">
-			<div className="bg-#ffffff19 px-4 py-7 sm:px-12 md:flex md:items-center md:justify-between">
-				<div>
-					Western Esports Gaming Association 2023
-					{/*Add Discord, TikTok*/}
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="bg-purple-800 text-white">
+      <div className="bg-#ffffff19 px-4 py-7 sm:px-12 md:flex md:items-center md:justify-between">
+        <div>
+          Western Esports Gaming Association 2023
+          {/*Add Discord, TikTok*/}
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

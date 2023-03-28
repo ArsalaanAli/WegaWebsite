7b3bab0862adbd5f6b732fea322dc5e1597268b1
Kyle Chen <kchen727@uwo.ca>
2023-03-28 01:19:03 -0400

    Pagination changes. Starting the teams page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,45 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "../public/favicon.ico";
+import Link from "next/link";
 
 const navbar = () => {
-  return (
-    <div>
-      <nav className="flex items-center justify-between px-5 py-3">
-        <div className="flex items-center p-0">
-          <Image
-            src="/WegaLogoSVG.svg"
-            alt="wega logo"
-            width={80}
-            height={80}
-            className=""
-          />
-          <h1 className="text-2xl font-bold">WEGA</h1>
-        </div>
-        <div className="flex items-center p-0">
-          <ul className="flex items-center space-x-6">
-            <li className="group cursor-pointer text-xl font-bold  transition duration-300">
-              TEAMS
-              <span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
-            </li>
-            <li className="group cursor-pointer text-xl font-bold  transition duration-300">
-              ABOUT US
-              <span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
-            </li>
-            <li className="group cursor-pointer text-xl font-bold  transition duration-300">
-              FAQ
-              <span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+	return (
+		<div>
+			<nav className="flex items-center justify-between px-5 py-3">
+				<div className="flex items-center p-0">
+					<Link href="/">
+						<Image
+							src="/WegaLogoSVG.svg"
+							alt="wega logo"
+							width={80}
+							height={80}
+							className=""
+						/>
+					</Link>
+					<Link href="/">
+						<h1 className="text-2xl font-bold">WEGA</h1>
+					</Link>
+				</div>
+				<div className="flex items-center p-0">
+					<ul className="flex items-center space-x-6">
+						<li className="group cursor-pointer text-xl font-bold  transition duration-300">
+							<Link href="../Teams">TEAMS </Link>
+							<span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
+						</li>
+						<li className="group cursor-pointer text-xl font-bold  transition duration-300">
+							<Link href="/About">ABOUT US</Link>
+							<span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
+						</li>
+						<li className="group cursor-pointer text-xl font-bold  transition duration-300">
+							<Link href="/FAQ">FAQ </Link>
+							<span className="block h-0.5 max-w-0 bg-purple-600 transition-all duration-500 group-hover:max-w-full" />
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
 };
 
 export default navbar;
---
+++ pages/Teams.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+const Team = () => {
+	return (
+		<div className="h-full">
+			<Navbar />
+			Team
+			<Footer />
+		</div>
+	);
+};
+
+export default Team;
